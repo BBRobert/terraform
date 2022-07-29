5c94133fe48b1f77abea189fb94dccd120983142
Robert Sirbu <robert.sirbu@pearson.com>
2022-07-29 08:20:13 +0300

    test commit1 master

--- locals.tf
+++ locals.tf
@@ -3,3 +3,4 @@ locals {
   availability_zones = ["${var.region}a", "${var.region}b", "${var.region}c"]
   ansible_run = "${var.ansible_command} -i ${var.ansible_inventory} ${var.ansible_playbook}"
 }
+
